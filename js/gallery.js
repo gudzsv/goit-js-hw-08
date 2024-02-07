7eaab9f0f024dee452e435281979ee4dfc31d9bd
Serhii Hudzenko <gudzsvmail@gmail.com>
2024-02-07 16:38:08 +0200

    complete to check

---
+++ js/gallery.js
@@ -0,0 +1,64 @@
+import images from './data.js';
+
+const gallery = document.querySelector('.gallery');
+
+const markup = images.map(({ preview, original, description }) => {
+  return `<li class="gallery-item">
+  <a class="gallery-link" href="${removeFirstLaseChar(original)}">
+    <img
+      class="gallery-image"
+      src="${removeFirstLaseChar(preview)}"
+      data-source="${removeFirstLaseChar(original)}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+});
+
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName === 'IMG') {
+    console.log(event.target.dataset.source);
+
+    openModal(event.target.dataset.source);
+  }
+});
+
+function openModal(src) {
+  const instance = basicLightbox.create(
+    `
+    	<img src="${src}" width="1112" height="640">
+    `,
+    {
+      className: 'modal',
+
+      onShow: instance => {
+        document.addEventListener('keydown', event => {
+          if (event.code === 'Escape') {
+            instance.close();
+          }
+        });
+      },
+
+      onClose: instance => {
+        document.addEventListener('keydown', event => {
+          if (
+            event.code === 'Enter' ||
+            event.code === 'NumpadEnter' ||
+            event.code === 'Space'
+          ) {
+            instance.show();
+          }
+        });
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function removeFirstLaseChar(string) {
+  return string.slice(1, string.length - 1);
+}
